5dee424769f041766866c43b5d0541a8d311ff41
colino17 <51090962+colino17@users.noreply.github.com>
2022-02-24 10:24:40 -0400

    Update jobs.sh

--- jobs.sh
+++ jobs.sh
@@ -7,20 +7,6 @@ echo "Creating DUMMY XMLTV data..."
 sleep 1
 fi
 
-# ZAP2XML - PRIMARY INSTANCE
-if [ $Z1=true ]; then
-echo "Retrieving XMLTV data for $ZUSER1..."
-/zap2xml.pl -u $ZUSER1 -p $ZPASS1 -U -o /xmltv/$ZXML1 $ZARG1
-sleep 1
-fi
-
-# ZAP2XML - SECONDARY INSTANCE
-if [ $Z2=true ]; then
-echo "Retrieving XMLTV data for $ZUSER2..."
-/zap2xml.pl -u $ZUSER2 -p $ZPASS2 -U -o /xmltv/$ZXML2 $ZARG2
-sleep 1
-fi
-
 # TOONAMI AFTERMATH
 if [ $TOONAMI=true ]; then
 echo "Retrieving M3U and XMLTV data from TOONAMI AFTERMATH..."
@@ -35,13 +21,17 @@ python3 /ustv.py
 sleep 1
 fi
 
-# UPDATE XTEVE VIA API
-echo "Updating XTEVE playlists and XMLTV..."
-curl -X POST -d '{"cmd":"update.m3u"}' http://$XIP:$XPORT/api/
+# UPDATE TVH VIA API
+if [ $TVH=true ] && [ $USTV=true ]; then
+echo "Scanning Channels..."
+curl http://$TVH_USER:$TVH_PASS@$TVH_IP:9981/api/mpegts/network/scan?uuid=$USTV_UUID
 sleep 5
-curl -X POST -d '{"cmd":"update.xmltv"}' http://$XIP:$XPORT/api/
+fi
+
+if [ $TVH=true ]; then
+echo "Updating EPG..."
+curl http://$TVH_USER:$TVH_PASS@$TVH_IP:9981/api/epggrab/internal/rerun?rerun=1
 sleep 5
-curl -X POST -d '{"cmd":"update.xepg"}' http://$XIP:$XPORT/api/
-sleep 5
+fi
 
 exit
