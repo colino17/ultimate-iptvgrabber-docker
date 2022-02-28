f2260416fc3aacbad3810e1feaeba52533dce73c
colino17 <51090962+colino17@users.noreply.github.com>
2022-02-28 15:17:19 -0400

    Create nhl.sh

---
+++ scripts/nhl.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# LAZYSTREAM
+if [ $NHL=true ]; then
+echo "Retrieving M3U and XMLTV data from LAZYSTREAM..."
+lazystream generate xmltv /xmltv/nhl.xml --sport nhl --exclude-feeds "COMPOSITE" --channel-prefix "NHL LIVE" --start-channel "1000"
+sleep 5
+fi
+
+# UPDATE TVH VIA API
+if [ $TVH=true ] && [ $USTV=true ]; then
+echo "Scanning Channels..."
+curl http://$TVH_USER:$TVH_PASS@$TVH_IP:9981/api/mpegts/network/scan?uuid=$NHL_UUID
+sleep 5
+fi
+
+if [ $TVH=true ]; then
+echo "Updating EPG..."
+curl http://$TVH_USER:$TVH_PASS@$TVH_IP:9981/api/epggrab/internal/rerun?rerun=1
+sleep 5
+fi
+
+exit
