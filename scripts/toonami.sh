78c47f0bff76739a1a559b9655d84efa4eb0d5e2
colino17 <51090962+colino17@users.noreply.github.com>
2022-03-01 16:29:48 -0400

    Add logic to merge dummy and toonami

--- scripts/toonami.sh
+++ scripts/toonami.sh
@@ -7,6 +7,28 @@ python3 /toonami/toonami.py
 sleep 5
 fi
 
+# DUMMY XMLTV
+if [ $DUMMY=true ]; then
+echo "Creating DUMMY XMLTV data..."
+/bin/bash /dummy/dummyxmltv.sh
+sleep 1
+fi
+
+# MERGE XMLTV
+if [ $DUMMY=true && $TOONAMI=true ]; then
+echo "Merging XMLTV data..."
+tv_merge -i /tmp/dummy.xml -m toonami.xml -o /tmp/dummy.xml
+sleep 1
+elif [ $DUMMY=true && $TOONAMI=false ]; then
+echo "Moving XMLTV data..."
+cp /tmp/dummy.xml /xmltv/xmltv.xml
+sleep 1
+elif [ $DUMMY=false && $TOONAMI=true ]; then
+echo "Moving XMLTV data..."
+cp /tmp/toonami.xml /xmltv/xmltv.xml
+sleep 1
+fi
+
 # UPDATE TVH VIA API
 if [ $TVH=true ]; then
 echo "Updating EPG..."
