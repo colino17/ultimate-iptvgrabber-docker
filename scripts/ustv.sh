9bbdb1b9e5bf2d4ad0a5c6ee3f7910119657ddcd
colino17 <51090962+colino17@users.noreply.github.com>
2022-03-02 12:40:36 -0400

    Update ustv.sh

--- scripts/ustv.sh
+++ scripts/ustv.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
 # USTVGO
-if [ $USTV=true ]; then
+if [ $USTV = "true" ]; then
 echo "Retrieving M3U from USTVGO..."
 python3 /ustv/ustv.py
 sleep 1
 fi
 
 # UPDATE TVH VIA API
-if [ $TVH=true ] && [ $USTV=true ]; then
+if [ $TVH = "true" ] && [ $USTV=true ]; then
 echo "Scanning Channels..."
 curl http://$TVH_USER:$TVH_PASS@$TVH_IP:9981/api/mpegts/network/scan?uuid=$USTV_UUID
 sleep 5
 fi
 
-if [ $TVH=true ]; then
+if [ $TVH = "true" ]; then
 echo "Updating EPG..."
 curl http://$TVH_USER:$TVH_PASS@$TVH_IP:9981/api/epggrab/internal/rerun?rerun=1
 sleep 5
