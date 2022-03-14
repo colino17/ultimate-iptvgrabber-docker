6d2c5e157184cae607b6f815f32cdde2819ae68b
colino17 <51090962+colino17@users.noreply.github.com>
2022-03-13 23:55:02 -0300

    Create individual M3U8 files for each USTV channel
    * Update ustv.sh
    
    * Update ustv.sh
    
    * Update ustv.sh

--- scripts/ustv.sh
+++ scripts/ustv.sh
@@ -5,7 +5,7 @@ if [ $USTV = "true" ]; then
 echo "Retrieving M3U from USTVGO..."
 python3 /ustv/ustv.py
 sleep 1
-echo "Creating individual channels M3Us..."
+echo "Creating static individual M3U8 for each channel..."
   for i in {2..120..2}
   do
      B=$((i+1))
@@ -13,18 +13,14 @@ echo "Creating individual channels M3Us..."
      lineB=$(sed -n "${B}p" "/playlists/ustvgo.m3u")
      name=$(cut -d "," -f2- <<< "$lineA")
      echo "#EXTM3U
-     $lineA
-     $lineB" > "/playlists/$name.m3u"
+#EXT-X-VERSION:3
+#EXT-X-STREAM-INF:BANDWIDTH=1383144,RESOLUTION=640x360
+$lineB" > "/playlists/$name.m3u8"
   done
-fi
-
-# UPDATE TVH VIA API
-if [ $TVH = "true" ] && [ $USTV=true ]; then
-echo "Scanning Channels..."
-curl http://$TVH_USER:$TVH_PASS@$TVH_IP:9981/api/mpegts/network/scan?uuid=$USTV_UUID
 sleep 5
 fi
 
+# UPDATE EPG VIA TVH API CALLS
 if [ $TVH = "true" ]; then
 echo "Updating EPG..."
 curl http://$TVH_USER:$TVH_PASS@$TVH_IP:9981/api/epggrab/internal/rerun?rerun=1
